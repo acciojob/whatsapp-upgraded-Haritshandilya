594936e5a96e1bc3d510810396255ef104a1f697
Harit <haritshandilya.beas@gmail.com>
2023-07-22 23:23:55 +0530

    Trying

--- src/main/java/com/driver/Group.java
+++ src/main/java/com/driver/Group.java
@@ -1,7 +1,29 @@
 package com.driver;
 
 public class Group {
+    public Group(String name, int i) {
+        this.name = name;
+        this.numberOfParticipants = i;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumberOfParticipants() {
+        return numberOfParticipants;
+    }
+
+    public void setNumberOfParticipants(int numberOfParticipants) {
+        this.numberOfParticipants = numberOfParticipants;
+    }
+
     private String name;
     private int numberOfParticipants;
+
 
 }
--- src/main/java/com/driver/Message.java
+++ src/main/java/com/driver/Message.java
@@ -3,7 +3,40 @@ package com.driver;
 import java.util.Date;
 
 public class Message {
+
     private int id;
     private String content;
     private Date timestamp;
+
+    public Message(int messageId, String content) {
+        this.id = messageId;
+        this.content = content;
+        timestamp = new Date();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
+
+
 }
